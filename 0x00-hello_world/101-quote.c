0aacfe97d00e837e0feee2258b2a6ca1a225ed6c
rachida <rachidaanjr@gmail.com>
2023-07-13 19:56:44 -0700

    ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,10 @@
 #include <unistd.h>
 #define MESSAGE "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
 	write(2, MESSAGE, sizeof(MESSAGE) - 1);
