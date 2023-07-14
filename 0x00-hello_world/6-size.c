fe906ab58b69436e7c0c4a47c4c11158a7f40165
rachida <rachidaanjr@gmail.com>
2023-07-13 19:21:17 -0700

    ok

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Size of char: %lu byte(s)\n", sizeof(char));
+    printf("Size of int: %lu byte(s)\n", sizeof(int));
+    printf("Size of float: %lu byte(s)\n", sizeof(float));
+    printf("Size of double: %lu byte(s)\n", sizeof(double));
+    printf("Size of long double: %lu byte(s)\n", sizeof(long double));
+
+    return 0;
+}
